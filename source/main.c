7aa6b77ab1371ec4ebc8083f5a8b9d373687af5e
Manuel Limones <limonesu.me@gmail.com>
2015-02-05 03:55:02 -0600

    Add  timer example for system clock test

--- source/main.c
+++ source/main.c
@@ -1,9 +1,22 @@
 #include "TM4C123.h"                    // Device header
+#include "timer.h"
 
-unsigned speed;
-
+unsigned int speed;
+unsigned int idle_eventCounter;
 int main(){
 	//SystemCoreClockUpdate();
 	speed=SystemCoreClock;
-	while(1);
+	timerInit();
+	timerSet(1814,0);//interrupt every (0+1)*2000 counts 
+	timerIRQConfig(1);
+	ledsInit();
+	GPIOF->DATA|=(0x1<<1)|(0x1<<2)|(0x1<<3);
+	timerEnable(1);
+	while(1){
+		if(timer_eventCounter>=(44100/2)){
+			timer_eventCounter=0;
+			GPIOF->DATA^=(0x1<<1)|(0x1<<2)|(0x1<<3);
+		}
+		idle_eventCounter++;
+	}
 }
